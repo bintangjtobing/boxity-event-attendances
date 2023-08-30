dfb9c32dfb7297bba3ba63ea6e7fa77c08e1ee30
LuckyDevelop <nikovicky9@gmail.com>
2023-08-30 14:38:55 +0700

    udpate participant

--- app/Participants.php
+++ app/Participants.php
@@ -10,21 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 class Participants extends Model
 {
     protected $table = 'participants';
-    protected $fillable = [
-        'participant_id',
-        'event_id',
-        'name',
-        'email',
-        'jabatan',
-        'no_hp',
-        'instansi',
-        'alamat_instansi',
-        'tanggal_kedatangan',
-        'penginapan',
-        'tanggal_kembali',
-        'qr_code',
-        'qr_code_file_name'
-    ];
+    protected $guarded = [];
 
     public function Event() {
         return $this->belongsTo(Events::class, 'event_id');
